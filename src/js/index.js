33ffc504d1b3123a4f83eee30472976cc01fcdcd
Lucas Carvalho Fernandes <lucascf000@gmail.com>
2022-10-03 12:23:25 -0300

    .

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,9 @@ const work = document.getElementById("work")
 const ability = document.getElementById("ability")
 const contact = document.getElementById("contact")
 
+let nav = document.querySelectorAll("ul")[0].children
+
 let icon = document.querySelectorAll(".fa-x")
-
-
 
 function openTab(way) {
     way.classList.toggle("hidden")
@@ -21,5 +21,11 @@ icon.forEach(icon => {
     icon.addEventListener("click", event => {
         var tab = event.path[1]
         openTab(tab)
+    })
+})
+
+nav.forEach(nav => {
+    nav.addEventListener("click", event => {
+        console.log("clicou")
     })
 })
